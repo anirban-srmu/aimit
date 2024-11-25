78d301cbaa5aac12b1be8eacd77b9d14eec69955
Anirban <anirban@bodhyaan.com>
2024-11-25 15:26:56 +0530

    conditional rendering using ternary operator

--- reactproject/src/LoginStatus.js
+++ reactproject/src/LoginStatus.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function LoginStatus({isLoogedIn}){
-    if (isLoogedIn){
-        return <h1>Welcome Member.</h1>;
-    }else{
-        return <h1>Welcome Guest.</h1>;
-    }
+    // if (isLoogedIn){
+    //     return <h1>Welcome Member.</h1>;
+    // }else{
+    //     return <h1>Welcome Guest.</h1>;
+    // }
+    return(<div>
+        {isLoogedIn ? <h1>Welcome Member.</h1> : <h1>Welcome Guest.</h1>}
+    </div>);
 }
 
 export default LoginStatus;
