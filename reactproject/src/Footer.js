9efb38ae31ddc3066ddb7218cee4a9784c7d4546
Anirban <anirban@bodhyaan.com>
2024-11-25 12:48:05 +0530

    functional components added footer, greeting and WelcomeMessage

---
+++ reactproject/src/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Footer(){
+    return <footer> © 2024 My React App</footer>
+}
+
+export default  Footer;
---
+++ reactproject/src/WelcomeMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function WelcomeMessage(){
+    return <p>Welcome to the React Application!</p>;
+}
+
+export default WelcomeMessage;
