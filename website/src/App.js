8c87e900ff8f485b93733f87415e721515ae5821
Anirban <anirban@bodhyaan.com>
2024-11-26 14:41:55 +0530

    router based app updated

---
+++ website/src/App.js
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Router>
+  );
+}
+
+function Home(){
+  return <h1>Welcome to the Home Page.</h1>;
+}
+
+function About(){
+  return <h1>About Us</h1>;
+}
+
+export default App;
