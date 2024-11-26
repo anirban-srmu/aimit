07a5074020102b09fa9cdd5f315c491acb37e1e8
Anirban <anirban@bodhyaan.com>
2024-11-26 13:57:35 +0530

    loginform component is updated

--- login/src/LoginForm.js
+++ login/src/LoginForm.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function LoginForm(){
-    const [username, setUsername] = React.useState('');
+    const [formData, setFormData] = React.useState({
+        username:'',
+        password: '',
+    });
 
     function handleSubmit(event){
         event.preventDefault();
-        alert(`Username: ${username}`);
+        alert(`User: ${formData.username},Password: ${formData.password}`);
+        //call an API which will take user namd password as input and return true or flase along with a tocken
+    }
+    function handleChange(event){
+        const {name,value} = event.target;
+        setFormData({
+            ...formData,
+            [name]:value,
+        });
     }
 
     return(
@@ -14,8 +25,18 @@ function LoginForm(){
                 Username:
                 <input
                 type ="text"
-                value={username}
-                onChange={(e)=>setUsername(e.target.value)}
+                name= "username"
+                value={formData.username}
+                onChange={handleChange}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                type ="password"
+                name ="password"
+                value={formData.password}
+                onChange={handleChange}
                 />
             </label>
             <button type='submit'>Sumbit</button>
